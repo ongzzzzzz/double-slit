63d97f8321d0938ca99812b73a0f240f5aef6c1f
Ong Zhi Zheng <ongzhizheng@gmail.com>
2021-09-21 16:50:39 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,111 @@
+let topMost;
+let bottomMost;
+let leftMost;
+let rightMost;
+
+// in pixels
+let distanceBetweenSlits = 100;
+let distanceToScreen = 50;
+let wavelength = 20;
+let slitWidth = 10;
+
+let dSlider; // slit separation
+let lSlider; // dist to screen
+let wSlider; // wavelength 
+
+let n;
+let k = 0.5;
+let modulo;
+let pointColor;
+
+let wavesToDraw = 20;
+
+let inputRadius = 20;
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	background(255);
+
+	topMost = height / 2;
+	bottomMost = -height / 2;
+	leftMost = -width / 2;
+	rightMost = width / 2;
+
+	dSlider = createSlider(1, height / 2, 100)
+	lSlider = createSlider(1, width / 2, width / 4)
+	wSlider = createSlider(1, 300, 20)
+}
+// 60, 330, 26
+function draw() {
+	background(0);
+	translate(width / 2, height / 2)
+	applyMatrix(1, 0, 0, -1, 0, 0, 0)
+
+	distanceBetweenSlits = dSlider.value();
+	distanceToScreen = lSlider.value();
+	wavelength = wSlider.value();
+
+	drawSlits(-0.5 * distanceToScreen)
+	drawScreen(0.5 * distanceToScreen)
+	drawWaves(-0.5 * distanceToScreen)
+}
+
+function drawSlits(x) {
+	stroke(255)
+	line(x, topMost, x, (0.5 * distanceBetweenSlits) + slitWidth)
+	line(x, (0.5 * distanceBetweenSlits), x, -(0.5 * distanceBetweenSlits))
+	line(x, -(0.5 * distanceBetweenSlits) - slitWidth, x, bottomMost)
+}
+
+function drawScreen(x) {
+	for (let y = bottomMost; y < topMost; y++) {
+		n = ((distanceBetweenSlits * y) / (distanceToScreen * wavelength));
+
+		// to make sure the modulo is positive
+		modulo = (n % k > 0) ? (n % k) : ((n % k) + k)
+
+		// check .draw for details
+		if (Math.ceil(n / k) % 2 == 0) {
+			pointColor = map(modulo, 0, k, 0, 255)
+		} else {
+			pointColor = map(modulo, 0, k, 255, 0)
+		}
+		stroke(pointColor)
+		point(x, y)
+	}
+}
+
+function drawWaves(x) {
+	noFill();
+	stroke(150)
+
+	for (let i = 1; i <= wavesToDraw; i++) {
+		arc(x, 0.5 * (distanceBetweenSlits + slitWidth),
+			i * wavelength, i * wavelength, -PI / 2, PI / 2)
+	}
+
+	for (let i = 1; i <= wavesToDraw; i++) {
+		arc(x, -0.5 * (distanceBetweenSlits + slitWidth),
+			i * wavelength, i * wavelength, -PI / 2, PI / 2)
+	}
+
+}
+
+function mouseDragged() {
+	let x = -0.5 * distanceToScreen;
+
+	// changed origin to be center of screen
+	let mX = mouseX - width/2;
+	let mY = height/2 - mouseY;
+	
+	if (dist(mX, mY,
+		x, 0.5 * (distanceBetweenSlits + slitWidth)) <= inputRadius
+		|| dist(mX, mY,
+			x, -0.5 * (distanceBetweenSlits + slitWidth)) <= inputRadius) {
+		console.log(mX, mY)
+		dSlider.value( 2*(dist(x, mY, x, 0)) )
+	}
+
+	if ()
+	return false;
+}
